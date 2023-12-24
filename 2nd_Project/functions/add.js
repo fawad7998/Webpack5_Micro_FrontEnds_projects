ea49b558a45d71d2044cd6758418ab358faecb20
Fawad Ahmad <fawad7998@gmail.com>
2023-12-24 18:23:15 +0500

    Webpack 2 projects uploaded

---
+++ 2nd_Project/functions/add.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  return a + b;
+}
+function addnmultiply(a, b, c) {
+  return (a + b) * c;
+}
+
+module.exports = {
+  add,
+  addnmultiply,
+};
---
+++ 2nd_Project/main.js
@@ -0,0 +1,5 @@
+const { add, addnmultiply } = require('./functions/add');
+
+const output = addnmultiply(1, 2, 3);
+add(2, 3);
+console.log(output);
